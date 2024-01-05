94fb904edff8b0daf046e0e75d412323753b6193
Vu Hong Thai <thai.vh@evotek.vn>
2024-01-05 18:45:14 +0700

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { caculateStart } from './logic';
 
@@ -7,15 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <button
           className="App-link"
           onClick={() => caculateStart()}
         >
-          Learn React
+          Tính toán
         </button>
       </header>
     </div>
